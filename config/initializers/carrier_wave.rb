4da41d009f865f99269aecb666327d91796e7054
Ahaupt3 <alexhaupt3@gmail.com>
2019-02-27 23:41:43 -0800

    updated AWS config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,18 @@
 if Rails.env.production?
     CarrierWave.configure do |config|
+      config.root = Rails.root.join('tmp')
+      config.cache_dir = 'carrierwave'
+
       config.fog_credentials = {
-        # Configuration for Amazon S3
-        :provider              => 'AWS',
+        :provider               => 'AWS',
         :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
         :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-        :aws_region            => ENV['S3_REGION']
+        :region                 => 'us-west-1',
+        :host                   => 'alex-ror-003.herokuapp.com/',
+        :endpoint               => 'https://alex-ror-003.herokuapp.com/:8080'
       }
-      config.fog_directory     =  ENV['S3_BUCKET']
+      config.fog_directory  = ENV['S3_Bucket']
+      config.fog_public     = false
+      config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
     end
-  end
+end
