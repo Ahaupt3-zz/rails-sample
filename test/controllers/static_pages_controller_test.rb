0013a4104af7f2647736bc81cabcae03ce4e36e0
Ahaupt3 <alexhaupt3@gmail.com>
2019-03-02 22:26:18 -0800

    updated static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,12 +9,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   assert_select "title", "RoR Sample"
 end
 
-  test "should get help" do
-    get help_path
-    assert_response :success
-    assert_select "title", "Help | RoR Sample"
-  end
-
   test "should get about" do
     get about_path
     assert_response :success
